99cdfaa6d0366e942df8e27a5e855aa93c577767
League Student <league@iMac-23.attlocal.net>
2019-11-13 16:56:21 -0800

    DiceRoll, Validation, Magic8Ball

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
 		
 		Random randomMaker = new Random();
 		
@@ -17,7 +18,18 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+			if (randomNumber == 0) {
+				JOptionPane.showMessageDialog(null, "Nice shoes!");
+			} else if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "Nice clothes!");
+			} else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "Cool glasses!");
+			} else if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "I like your hair!");
+			} else {
+				JOptionPane.showMessageDialog(null, "Cool hat!");
+			}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -1,35 +1,48 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
 package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+	public static void main(String[] args) {
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+		Random gen = new Random();
+		
+		int num = gen.nextInt(4);
+		// 3. Print out this variable
+		System.out.println(num);
+		// 4. Get the user to enter a question for the 8 ball
+		String question = JOptionPane.showInputDialog("What would you like to ask the magic 8 ball?");
+		// 5. If the random number is 0
+		if (num == 0) {
+			JOptionPane.showMessageDialog(null, "Yes");
+		}
+		// -- tell the user "Yes"
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 6. If the random number is 1
+		if (num == 1) {
+			JOptionPane.showMessageDialog(null, "No");
+		}
+		// -- tell the user "No"
 
-	// 3. Print out this variable
+		// 7. If the random number is 2
+		if (num == 2) {
+			JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+		}
+		// -- tell the user "Maybe you should ask Google?"
 
-	// 4. Get the user to enter a question for the 8 ball
+		// 8. If the random number is 3
+		if (num == 3) {
+			JOptionPane.showMessageDialog(null, "What do you think?");
+		}
+		// -- write your own answer
 
-	// 5. If the random number is 0
-
-	// -- tell the user "Yes"
-
-	// 6. If the random number is 1
-
-	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+	}
 }
